bd2c1d1c0ce7a210af5378578cf97db503bd2c31
Aisha <64094719+hmmychoice@users.noreply.github.com>
2024-06-10 09:32:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 import os
-
-google_api_key = os.environ.get('GOOGLE_API_KEY', 'AIzaSyAtNQ0N9i16Ad3iBiHPLRJq2OJj5T79P6g')
 repo_id = "mistralai/Mistral-7B-Instruct-v0.2"
 model_name = "sentence-transformers/all-MiniLM-L6-v2"
 
-os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = "Hugging Face Key"
